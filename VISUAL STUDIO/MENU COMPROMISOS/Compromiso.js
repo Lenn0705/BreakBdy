d6bf80dbead5430aa97c4b3408d86fd4333f3434
Samuelleon2810 <samuelleon.acostac@gmail.com>
2023-09-16 11:34:00 -0500

    funcion botones

---
+++ VISUAL STUDIO/MENU COMPROMISOS/Compromiso.js
@@ -0,0 +1,14 @@
+document.getElementById("botonRedireccionar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20COMPROMISOS/FORMULARIO%20CREAR/CrearCompromiso.html";
+});
+
+document.getElementById("botonRedireccionarEliminar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20COMPROMISOS/FORMULARIO%20ELIMINAR/EliminarCompromiso.html";
+});
+
+document.getElementById("botonRegresar").addEventListener("click", function() {
+    history.back();
+})
+
+
+//../FORMULARIO%20CREAR/CrearCompromiso.html
