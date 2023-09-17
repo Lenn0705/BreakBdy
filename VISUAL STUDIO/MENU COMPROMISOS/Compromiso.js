b8b40b3977125de68181f43691005685344327c1
Samuelleon2810 <samuelleon.acostac@gmail.com>
2023-09-17 15:45:03 -0500

    modificaciones menus con javascript

--- VISUAL STUDIO/MENU COMPROMISOS/Compromiso.js
+++ VISUAL STUDIO/MENU COMPROMISOS/Compromiso.js
@@ -2,13 +2,15 @@ document.getElementById("botonRedireccionar").addEventListener("click", function
     window.location.href = "/VISUAL%20STUDIO/MENU%20COMPROMISOS/FORMULARIO%20CREAR/CrearCompromiso.html";
 });
 
+document.getElementById("botonVolver").addEventListener("click", function() {
+    window.location.href = "../VISUAL%20STUDIO/MENU%20COMPROMISOS/Compromiso.html";
+});
+
 document.getElementById("botonRedireccionarEliminar").addEventListener("click", function() {
     window.location.href = "/VISUAL%20STUDIO/MENU%20COMPROMISOS/FORMULARIO%20ELIMINAR/EliminarCompromiso.html";
 });
 
-document.getElementById("botonRegresar").addEventListener("click", function() {
-    history.back();
-})
+
 
 
 //../FORMULARIO%20CREAR/CrearCompromiso.html
---
+++ VISUAL STUDIO/MENU EVENTO/Evento.js
@@ -0,0 +1,7 @@
+document.getElementById("botonRedireccionar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20EVENTO/FORMULARIO%20CREAR/CrearEvento.html";
+});
+
+document.getElementById("botonRedireccionarEliminar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20EVENTO/FORMULARIO%20ELIMINAR%20EVENTO/EliminarEvento.html";
+});
