93bb5a439655a45955538ac5cc9add05e8fd970c
Lenn0705 <142263701+Lenn0705@users.noreply.github.com>
2023-09-17 21:04:53 -0500

    adición y modificación de elementos
    *se adiciona la carpeta de MENU TAREAS con sus respectivos archivos y subcarpetas CREAR TAREA, EDITAR TAREA y ELIMINAR TAREA.
    *Se modifican los redireccionamientos de los botones para ir atras o regresar.
    *Se modifican los redireccionamientos de la barra de MENU PRINCIPAL
    *Se adiciona la carpeta MENU USUARIO, solo contiene archivos html y css SIN CODIGO.

---
+++ VISUAL STUDIO/MENU TAREAS/Tareas.js
@@ -0,0 +1,16 @@
+document.getElementById("botonRedireccionar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20TAREAS/CREAR%20TAREA/CrearTarea.html";
+});
+
+document.getElementById("botonVolver").addEventListener("click", function() {
+    window.location.href = "../VISUAL%20STUDIO/MENU%20TAREAS/Tareas.html";
+});
+
+document.getElementById("botonRedireccionarEliminar").addEventListener("click", function() {
+    window.location.href = "/VISUAL%20STUDIO/MENU%20TAREAS/ELIMINAR%20TAREA/EliminarTarea.html";
+});
+
+
+
+
+//../FORMULARIO%20CREAR/CrearCompromiso.html
