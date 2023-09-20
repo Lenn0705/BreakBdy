d5638f26c41f6a02acfc7a2d75265748a6eb7cba
Samuelleon2810 <samuelleon.acostac@gmail.com>
2023-09-20 16:17:40 -0500

    ultimos ajustes de los enlaces

--- VISUAL STUDIO/MENU PRINCIPAL/Menu.js
+++ VISUAL STUDIO/MENU PRINCIPAL/Menu.js
@@ -1,8 +1,11 @@
 const barra = document.getElementById("menu-barra");
 const expandir = document.getElementById("expandir");
 const texto = document.getElementById("texto");
+const barraDer = document.getElementById("menu-barra-der")
 
 expandir.addEventListener("click",function() {
     barra.style.width = "300px";
-    texto.style.display = "block";
+    barraDer.style.marginLeft = "600px";
+    
 })
+
